62da3ecdaadd3a336ba09bf5cfbbd7a0b24c10a7
vijay-yadav-3 <66308966+vijay-yadav-3@users.noreply.github.com>
2023-11-01 21:06:23 +0530

    Create schedule_and_points_refresh.py

---
+++ schedule_and_points_refresh.py
@@ -0,0 +1,23 @@
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("cricbuzz-cricket.p.rapidapi.com")
+headers = {
+    'X-RapidAPI-Key': "$RAPID_API_KEY",
+    'X-RapidAPI-Host': "cricbuzz-cricket.p.rapidapi.com"
+}
+conn.request("GET", "/series/v1/6732", headers=headers)
+res = conn.getresponse()
+data = res.read()
+#print(data.decode("utf-8"))
+f = open("sch.txt", "w")
+f.write(data.decode("utf-8"))
+f.close()
+
+conn.request("GET", "/stats/v1/series/6732/points-table", headers=headers)
+res = conn.getresponse()
+data = res.read()
+#print(data.decode("utf-8"))
+f = open("poin.txt", "w")
+f.write(data.decode("utf-8"))
+f.close()
